b4e9e82820be8a69ff0bea44e79ac87f5dc92ebe
cyrus01337 <cyrus21337@gmail.com>
2024-10-17 09:57:27 +0000

    Use externally built live-server executable

--- lua/custom/features/live-server-integration.lua
+++ lua/custom/features/live-server-integration.lua
@@ -1,9 +1,9 @@
 return {
     "barrett-ruth/live-server.nvim",
-    build = "npm add -g live-server",
     cmd = { "LiveServerStart", "LiveServerStop" },
     config = true,
     opts = {
         args = { "--no-browser", "--port=3000" },
+        install_path = "$FNM_DIR/aliase/default/bin",
     },
 }
