7e4bc6d9ca6cf35bf52e966197fc4caf5fde50dd
cyrus01337 <cyrus01337@gmail.com>
2024-11-21 13:03:16 +0000

    Only sort in visual mode

--- lua/custom/features/sorting.lua
+++ lua/custom/features/sorting.lua
@@ -1,10 +1,12 @@
+local mode = require("custom.lib.mode")
+
 return {
     "sQVe/sort.nvim",
     cmd = {
         "Sort",
     },
     keys = {
-        { "s", "<CMD>Sort<CR>" },
+        { "s", "<CMD>Sort<CR>", mode = mode.VISUAL },
     },
     config = true,
 }
